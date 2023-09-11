ceb340d4ae10055b40569a9f50c01eb894bd129d
thisissuman <43521912+thisissuman@users.noreply.github.com>
2023-09-11 23:21:00 +0530

    first commit

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { TbSearch } from "react-icons/tb";
+import { CgBackspace, CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
+import { Search } from "react-router-dom";
+import Cart from "../Cart/Cart";
+import { Context } from "../../utils/context";
+
+import "./Header.scss";
+
+const Header = () => {
+  const [scroled, setscroled] = useState(false);
+  const handelScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setscroled(true);
+    } else setscroled(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handelScroll);
+  }, []);
+
+  return (
+    <header className={`main-header ${scroled ? "sticky-header" : ""}`}>
+      <div className="header-component">
+        <ul className="left">
+          <li>Home</li>
+          <li>About</li>
+          <li>Category</li>
+        </ul>
+
+        <div className="center">DivineDealsIndia</div>
+        <div className="right">
+          <TbSearch />
+          <AiOutlineHeart />
+          <span className="cart-icon">
+            <CgShoppingCart />
+            <span>5</span>
+          </span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
